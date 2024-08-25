43ef4a3651ff33bdaa4ca4fea212103db9c436a8
RohitNegi59 <rohitnegi5910@gmail.com>
2024-08-25 22:19:56 +0530

    how many font is corrected

--- src/components/Apps/HowMany/Howmany.jsx
+++ src/components/Apps/HowMany/Howmany.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './Howmany.css' 
+import './Howmany.css'
 
 import ShowModel from '../GuessTheLetter/ShowModel';
 import img1 from '/src/Image/apple.jpg';
@@ -33,14 +33,14 @@ function Howmany() {
   const num = Math.floor(Math.random() * 9);
 
   const checking = (e) => {
-    if (e.target.value == num+1) {
+    if (e.target.value == num + 1) {
       setShowModel(true);
       setTimeout(() => {
         setShowModel(false);
         const newIndex = Math.floor(Math.random() * images.length);
         setCurrentIndex(newIndex);
       }, 3000);
-      
+
     }
     else {
       setShowModel(false);
@@ -49,46 +49,44 @@ function Howmany() {
     }
   };
 
-  // const close = () => {
-  //   setShowModel(false);
-  // }
 
 
   return (
-    
+
     <>
-         <div className='outerdiv'>
-<div className="Name">
-  How Many 
-</div>
+      <div className='outerdiv'>
+        <div className='Name_div'>
+          <div className="Name">
+            How Many
+          </div>
+        </div>
+        <div className="card_how_many">
 
-<div className="card_how_many">
+          {image(num + 1)}
 
-{image(num + 1)}
+        </div>
+        {showModel && <ShowModel />}
 
-</div>
-{showModel && <ShowModel/>}
+        <div className="Numbers">
+          <div className='row row1'>
+            <button className="circle Name " value="1" onClick={checking}>1</button>
+            <button className="circle Name" value="2" onClick={checking}>2</button >
+            <button className="circle Name" value="3" onClick={checking}>3</button >
+            <button className="circle Name" value="4" onClick={checking}>4</button >
+            <button className="circle Name" value="5" onClick={checking}>5</button >
+          </div>
+          <div className='row row2'>
+            <button className="circle Name" value="6" onClick={checking}>6</button >
+            <button className="circle Name" value="7" onClick={checking}>7</button >
+            <button className="circle Name" value="8" onClick={checking}>8</button >
+            <button className="circle Name" value="9" onClick={checking}>9</button >
+            <button className="circle Name" value="10" onClick={checking}>10</button >
+          </div>
 
-<div className="Numbers">
-  <div className='row row1'>
-<button  className="circle Name " value="1" onClick={checking}>1</button>
-<button  className="circle Name" value="2" onClick={checking}>2</button >
-<button  className="circle Name" value="3" onClick={checking}>3</button >
-<button  className="circle Name" value="4" onClick={checking}>4</button >
-<button  className="circle Name" value="5" onClick={checking}>5</button >
-</div>
-<div className='row row2'>
-<button  className="circle Name" value="6" onClick={checking}>6</button >
-<button  className="circle Name" value="7" onClick={checking}>7</button >
-<button  className="circle Name" value="8" onClick={checking}>8</button >
-<button  className="circle Name" value="9" onClick={checking}>9</button >
-<button  className="circle Name" value="10" onClick={checking}>10</button >
-</div>
-
-</div>
+        </div>
 
 
-     </div>
+      </div>
     </>
   )
 }
